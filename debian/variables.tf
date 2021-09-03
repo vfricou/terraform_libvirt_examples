1464ec1397ef30a0362f96beee698e08736349f8
Vincent FRICOU <vincent@fricouv.eu>
2021-09-03 10:50:13 +0200

    updt(debian): reformat terraform configuration to allow multiples vms provision

--- debian/variables.tf
+++ debian/variables.tf
@@ -2,47 +2,67 @@ variable "storage_pool_name" {
   type    = string
   default = "terraform"
 }
-variable "storage_pool_type" {
-  type    = string
-  default = "dir"
-}
 variable "storage_pool_path" {
   type    = string
   default = "/tmp/terraform_pool"
 }
-variable "image_path" {
+variable "storage_pool_type" {
   type    = string
-  default = "https://cdimage.debian.org/images/cloud/buster/20210329-591/debian-10-generic-amd64-20210329-591.qcow2"
+  default = "dir"
 }
 variable "image_format" {
   type    = string
   default = "qcow2"
 }
-variable "instance_name" {
-  type    = string
-  default = "tf-debian"
-}
 variable "instance_memory" {
-  type    = string
-  default = "2048"
+  type = list(string)
+  default = [
+    "2048"
+  ]
 }
 variable "instance_vcpu" {
-  type    = string
-  default = "1"
+  type = list(string)
+  default = [
+    "1"
+  ]
 }
 variable "instance_disk" {
-  type    = string
-  default = "21474836480"
+  type = list(string)
+  default = [
+    "21474836480"
+  ]
 }
-variable "instance_net_name" {
+variable "net_name" {
   type    = string
   default = "default"
 }
-variable "cloud_init_user_path" {
+variable "net_nameserver" {
   type    = string
-  default = "../cloud_init_configs/debian_user_config.yml"
+  default = "192.168.122.1"
 }
-variable "cloud_init_network_path" {
+variable "net_mask" {
   type    = string
-  default = "../cloud_init_configs/debian_network_config.yml"
+  default = "24"
+}
+variable "net_gw4" {
+  type    = string
+  default = "192.168.122.1"
+}
+variable "instance_name" {
+  type = list(string)
+  default = [
+    "tf-debian-1"
+  ]
+}
+variable "image_path" {
+  type = list(string)
+  default = [
+    "https://cdimage.debian.org/images/cloud/bullseye/latest/debian-11-generic-amd64.qcow2"
+  ]
+}
+variable "net_addr" {
+  type = list(string)
+  default = [
+    "192.168.122.200"
+  ]
 }
